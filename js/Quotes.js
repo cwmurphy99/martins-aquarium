5b16c87c732ee5c266593e373be64aa36acbcccf
Chris Murphy <theworldisyours@gmail.com>
2021-08-19 16:25:00 -0400

    render fish list

---
+++ js/Quotes.js
@@ -0,0 +1,26 @@
+const quotes = [
+    {
+        quote: "Florida isn't so much a place where one goes to reinvent oneself, as it is a place where one goes if one no longer wished to be found."
+    },
+    {
+        quote: "Don't worry about the world coming to an end today.  It is already tomorrow in Australia."
+    },
+    {
+        quote: "Holding onto anger is like drinking poison and expecting the other person to die."
+    }, 
+    {
+        quote: "Welcome to Ohio! HaHa, now you're stuck in Ohio!!"
+    },
+    {
+        quote: "If Jack Nicklaus can win the Masters at 46, I can win the Kentucky Derby at 54."
+    },
+    {  
+        quote: "In memory, everything seems to happen to music."
+    },
+    {
+        quote: "If everyday at work feels like a Friday, then you are doing what you were meant to do."
+    },
+    {
+        quote: "I don't know how long I have left, so I'm gonna get real weird with it."
+    }
+]
